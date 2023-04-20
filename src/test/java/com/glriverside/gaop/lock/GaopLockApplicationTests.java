cdd3a990d35f051e7899cba9c0d3eb9ffe67d7ff
‘陈水明’ <439752552@qq.com>
2023-04-20 18:37:39 +0800

    单元测试中始化批次

--- src/test/java/com/glriverside/gaop/lock/GaopLockApplicationTests.java
+++ src/test/java/com/glriverside/gaop/lock/GaopLockApplicationTests.java
@@ -1,13 +1,26 @@
 package com.glriverside.gaop.lock;
 
+import com.glriverside.gaop.lock.config.OrdersConfig;
+import com.glriverside.gaop.lock.service.LockManagerService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GaopLockApplicationTests {
-
+    @Autowired
+    private OrdersConfig ordersConfig;
+    @Autowired
+    private LockManagerService lockManagerService;
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        String order = ordersConfig.getOrder();
+        List<List<String>> batch = lockManagerService.stringToOrders(order);
+        LockManager lockManager = new LockManager();
+        lockManager.setBatchOrder(batch);
+        lockManager.printMap();
     }
 
 }
