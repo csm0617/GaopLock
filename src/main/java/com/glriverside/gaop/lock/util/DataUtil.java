ce85c46b16e6e93f86f8f8166ea5a7c0d90926a6
‘陈水明’ <439752552@qq.com>
2023-05-04 15:42:04 +0800

    新增工具类定义静态批次orders的全局变量

---
+++ src/main/java/com/glriverside/gaop/lock/util/DataUtil.java
@@ -0,0 +1,15 @@
+package com.glriverside.gaop.lock.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhai
+ * @version 1.0.0
+ * @ClassName DataUtil.java
+ * @createTime 2023年01月05日 10:55:00
+ */
+public class DataUtil {
+
+    public static Map<String, Integer> orders = new HashMap<>();
+}
